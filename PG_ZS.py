9860a290466d652b59a4d55b019d33f9385a0864
Zachst <33524272+Zachst@users.noreply.github.com>
2018-01-11 12:19:38 -0500

    Add files via upload

---
+++ PG_ZS.py
@@ -0,0 +1,23 @@
+import pyautogui as pg
+import time
+
+pg.hotkey('ctrl', 'winleft','d')
+
+pg.hotkey('winleft')
+
+pg.typewrite('chrome\n')
+
+pg.typewrite('espn.com\n')
+
+pg.hotkey('ctrl','t')
+
+pg.typewrite('nike.com\n')
+
+pg.hotkey('ctrl','t')
+
+pg.typewrite('youtube.com\n')
+
+pg.hotkey('ctrl','t')
+
+pg.typewrite('gmail.com\n')
+
